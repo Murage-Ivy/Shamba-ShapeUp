e661a7e9b0fbf11e313f4b136f6239a1b7938e9c
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-02 15:43:16 +0300

    Add a Login  component

--- src/Components/Landing/Login.js
+++ src/Components/Landing/Login.js
@@ -3,7 +3,8 @@ import React from "react";
 function Login () {
     return (
         <form className="form login">
-
+            <input type={"text"} placeholder="Enter your Name" value={"Ivy"}></input>
+            This is the login form
         </form>
     )
 }
