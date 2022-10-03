af1be1263560857712f3e7f3c6e900b0f86dcc31
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-03 12:09:00 +0300

    Create and style the header component

--- src/Components/Landing/Header/Header.js
+++ src/Components/Landing/Header/Header.js
@@ -4,8 +4,12 @@ import "./Header.css";
 function Header() {
   return (
     <header id="header">
-      <h1> Shamba Shape Up </h1> 
-      <p> Your Number One Plaform </p>
+      <div className="header-section">
+        <h1> Shamba Shape Up </h1> <p> Your Number One Plaform </p>
+        <button id="header-button">
+          <a href="#login">Get Started</a>
+        </button>
+      </div>
     </header>
   );
 }
