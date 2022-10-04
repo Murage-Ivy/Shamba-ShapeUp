6c030217b9fa8e6eb4682f116e04594719a735b2
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-04 17:17:52 +0300

    Refactor code

--- src/Components/Display/Posts.js
+++ src/Components/Display/Posts.js
@@ -4,16 +4,21 @@ function Posts() {
   return (
     <div className="posts">
       <div className="post">
-        <img src="" alt="" />
+        <img id="img"
+          src={
+            "https://images.unsplash.com/photo-1570042225831-d98fa7577f1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80"
+          }
+          alt="cow"
+        />
         <p>Hello there</p>
-        <div className="action-button">
+        <div className="action-btn">
           <div id="like-btn">
             <i className="fa-solid fa-heart"></i>
             <span>0 likes</span>
           </div>
           <div className="comment-btn">
             <i className="fa-regular fa-comment"></i>
-            <span>Commennt</span>
+            <span>Comment</span>
           </div>
         </div>
       </div>
