463544f5dc4a7edfb8351619e99acc0872f23ecb
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-04 23:12:02 +0300

    Refactor code

--- src/Components/Landing/Main./Main.js
+++ src/Components/Landing/Main./Main.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Main.css";
 import Navside from "../../Display/NavSide";
 import Posts from "../../Display/Posts";
 import PostForm from "../../Display/PostForm";
 
 function Main() {
-
+  const [posts, setPosts] = useState([]);
 
   return (
     <div className="main">
       {/* Displays the navbar and main part in a flex box */}
       <div className="nav-main">
-        <Navside />
-        <Posts />
+        <Navside setPosts={setPosts} />
+        <Posts posts={posts} />
       </div>
       <PostForm />
     </div>
