f6745d5a933cbc44197f6bc3c220eeb83ff5df61
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-05 18:15:56 +0300

    Update the page routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Main from "./Components/Landing/Main./Main";
 import SignUp from "./Components/Landing/SignUp/SignUp";
 import Header from "./Components/Landing/Header/Header";
 import Login from "./Components/Landing/Login/Login";
+import Main from "./Components/Landing/Main/Main"
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Header />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/main" element={<Main/>} />
         <Route path="/login" element={<Login />} />
-        <Route path="/main" element={<Main />} />
       </Routes>
       <br />
     </div>
