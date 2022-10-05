2ee01b3ded940c9633fc2a78d6d64a0bde0fb425
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-05 13:05:25 +0300

    Add an on submit event handler

--- src/Components/Landing/Login/Login.js
+++ src/Components/Landing/Login/Login.js
@@ -1,52 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  {
+    /* ===Sets initial state for react interanl== */
+  }
+  const [loginInfo, setLoginInfo] = useState({
+    first_name: "Ivy",
+    password: "fyguhjlk",
+  });
+
+  {/* ===Updates state when value changes=== */}
+  const handleLoginInfo = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setLoginInfo({
+      ...loginInfo,
+      [name]: value,
+    });
+  };
+
+  {
+    /* ===Handles submit login info=== */
+  }
+  const handleSubmitLoginInfo = (event) => {
+    event.preventDefault();
+    console.log(loginInfo);
+  };
+
   return (
     <div id="login">
-      {/* Holds login text */}
+      {/* ===Holds login text=== */}
       <div className="login-text">
-        <h2>Shamba Shape Up</h2>
+        <h2> Shamba Shape Up </h2>
         <p>
-          Connect with farmers and
-          <br></br>
+          Connect with farmers and <br />
           the world around you on Shamba Shape Up
         </p>
       </div>
 
-      {/* Login form */}
-      <form className="form login">
-        {/* Email input section */}
-        <label>Email</label>
-        <br></br>
+      {/*===== Login form =====*/}
+      <form 
+      className="form login" 
+      onSubmit={handleSubmitLoginInfo}>
+
+        {/* ===Email input section=== */}
+        <label> Email </label>
+
+        <br />
+
         <input
           type={"email"}
           placeholder="Enter Your Email"
           name="email"
           id="email-login"
+          autoComplete="on"
+          onChange={handleLoginInfo}
         />
 
-        <br></br>
-        {/* Password input section */}
-        <label>Password</label>
-        <br></br>
+        <br />
+
+        {/*=== Password input section=== */}
+        <label> Password </label>
+
+        <br />
+
         <input
           type={"password"}
           placeholder="Enter Your Email"
           name="password"
           id="password-login"
           autoComplete="current-password"
+          onChange={handleLoginInfo}
         />
 
-        <br></br>
-        {/* Submit button */}
+        <br />
+
+        {/* ====Submit button ====*/}
         <button type="submit" id="login-button">
           Login
         </button>
 
-        <br></br>
-        {/* Sign click label */}
-        <label id="signup">Don't have an account create one</label>
+        <br />
+
+        {/*=== Sign click label ===*/}
+        <Link to={"/signup"}>
+        <label id="signup"> Don 't have an account create one</label>
+        </Link>
       </form>
     </div>
   );
