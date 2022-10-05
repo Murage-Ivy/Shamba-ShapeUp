58ebb9016048816caa19d80987152402b6d4afe1
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-05 12:28:03 +0300

    Refactor css code for Header

--- src/Components/Landing/Header/Header.js
+++ src/Components/Landing/Header/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
+  /*=====Header Component=====*/
+
   return (
     <header id="header">
       <div className="header-section">
         <h1> Shamba Shape Up </h1> <p> Your Number One Plaform </p>
-        <button id="header-button">Get Started</button>
+        <Link to="/login">
+          <button id="header-button"> Get Started </button>
+        </Link>
       </div>
     </header>
   );
