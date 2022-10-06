a07ddafe90eee69ef252556cd29f85323589df53
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-06 09:41:02 +0300

    Refactor Navside component for readability

--- src/Components/Display/NavSide.js
+++ src/Components/Display/NavSide.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Topics from "../../Data";
 function Navside({ setPosts }) {
+  // sets up the initial state of the Navside components
   const [topic, setTopic] = useState("Animals");
+
+  // maps the topics to the corresponding li tag in the navigation component
   const topicsList = Topics.map((topic, index) => (
     <li onClick={handleTopicClick} className={topic} key={index}>
       {topic}
@@ -13,6 +16,7 @@ function Navside({ setPosts }) {
     setTopic(farmTopic);
   }
 
+  // useEffect should be called when the user loads the page and state changes accordingly
   useEffect(() => {
     const abortController = new AbortController();
     fetch(`http://localhost:4000/${topic}`)
@@ -20,6 +24,7 @@ function Navside({ setPosts }) {
       .then((data) => setPosts((prevData) => (prevData = data)))
       .catch((error) => console.log(error));
 
+    // cleanup function for the useEffect hook
     return () => {
       abortController.abort();
     };
