a0a4791239d0f19a9ef9d292489c2731f7b32d33
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-06 09:41:49 +0300

    Add a post description section for each post

--- src/Components/Display/Posts.js
+++ src/Components/Display/Posts.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 function Posts({ posts }) {
   const [likes, setLikes] = useState(0);
+
+  
   const postList = posts.map((post) => (
     <div key={post.id} className="post">
       <img src={post.image_url} alt={"farm"} />
       <p>{post.description}</p>
       <div className="action-btn">
-        <div  className={post.likes} onClick={onLikeClick}>
+        <div className={post.likes} onClick={onLikeClick}>
           <i className="fa-solid fa-heart"></i>
-          <span >{post.likes} likes</span>
+          <span>{post.likes} likes</span>
         </div>
         <div className="comment-btn">
           <i className="fa-regular fa-comment"></i>
@@ -19,9 +21,8 @@ function Posts({ posts }) {
   ));
 
   function onLikeClick(event) {
-    const likes = parseInt((event.target.textContent).split(' ')[0])
-  console.log(likes++)
-   
+    const likes = parseInt(event.target.textContent.split(" ")[0]);
+    console.log(likes++);
   }
 
   return <div className="posts">{postList}</div>;
